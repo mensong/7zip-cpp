d7a18411488983ec235e2407368433ec18231c3f
Gergul <coolcn@qq.com>
2023-12-13 00:19:27 +0800

    Compare -> compare

---
+++ z7pp-test.cpp
@@ -0,0 +1,19 @@
+﻿// z7pp-test.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <7zpp.h>
+
+using namespace SevenZip;
+
+int main()
+{
+    SevenZipLibrary lib;
+    lib.Load(L"D:\\Program Files\\7-Zip\\7z.dll");
+    SevenZipCompressor z(lib, L"D:\\mensong.7z");
+    z.AddDirectory(L"D:\\Programming\\Git\\7zip-cpp\\7zpp");
+    z.SetCompressionFormat(CompressionFormat::SevenZip);
+    z.SetCompressionLevel(CompressionLevel::Normal);
+    z.SetPassword(L"mensong");
+    z.DoCompress();
+}
